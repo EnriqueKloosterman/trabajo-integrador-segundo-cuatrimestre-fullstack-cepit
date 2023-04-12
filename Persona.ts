00dcac56102b24846c026bb7e588e14539b20204
EnriqueKloosterman <kloostermanen@gmail.com>
2023-04-12 09:39:32 -0300

    agregados  InterfacePersona y AbstractPersona

--- Persona.ts
+++ Persona.ts
@@ -1,18 +1,12 @@
-export class Persona{
-    private nombre: string;
-    private apellido: string;
-    private edad: number;
-    private dni: number;
-    private mail: string;
+import PersonaAbstarcta from "./AbstarctPersona";
 
-    public constructor(nombre: string, apellido: string, edad: number, dni: number, mail: string){
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
-        this.dni = dni;
-        this.mail = mail;        
+export class Persona extends PersonaAbstarcta{
+    public constructor(nombre: string, apellido: string, edad: number, dni: number, mail: string, contrato: boolean ){
+        super(nombre, apellido, edad, dni, mail, contrato)
     }
+}
+
+
     
 
 
-}
--- Profesor.ts
+++ Profesor.ts
@@ -3,8 +3,8 @@ import { Persona } from "./Persona";
 
 export class Profesor extends Persona{
     private materias: Materias[];
-    public constructor(nombre: string, apellido: string, edad: number, dni: number, mail: string){
-        super(nombre, apellido, edad, dni, mail);
+    public constructor(nombre: string, apellido: string, edad: number, dni: number, mail: string, contrato: boolean){
+        super(nombre, apellido, edad, dni, mail, contrato);
         this.materias = [];
     }
 }
